066db053071ce845cbfdd9eaba22ba518fd60144
omatrebym <ola.matre@bym.oslo.kommune.no>
2023-09-21 14:20:05 +0200

    Styr header ut ifra auth status

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import { PktHeader } from "@oslokommune/punkt-react";
+import { useSession } from "next-auth/react";
 
 export default function Header() {
+  const { data: session, status } = useSession();
+
   return (
     <PktHeader
       fixed
-      user={{
-        name: "Ola Matre",
-        shortname: "OM",
-        lastLoggedIn: new Date(),
-      }}
+      user={
+        status === "authenticated"
+          ? {
+              name: session?.user?.name ?? "Ikke innlogget",
+              shortname:
+                session?.user?.name
+                  ?.split(" ")
+                  .reduce((prev, curr) => prev + curr[0])
+                  .toLocaleUpperCase() ?? "NN",
+            }
+          : undefined
+      }
       serviceName="Klagebehandling"
       canChangeRepresentation={false}
     />
