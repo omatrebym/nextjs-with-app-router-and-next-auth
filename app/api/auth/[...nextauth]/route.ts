d207eb70b8a04bb84fd0329ee54041ca6244145a
omatrebym <ola.matre@bym.oslo.kommune.no>
2023-09-21 13:48:33 +0200

    Legg til auth provider

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth, { AuthOptions } from "next-auth";
+
+import OneLoginProvider from "next-auth/providers/onelogin";
+
+const authOptions: AuthOptions = {
+  providers: [
+    OneLoginProvider({
+      clientId: process.env.ONELOGIN_CLIENT_ID,
+      clientSecret: process.env.ONELOGIN_CLIENT_SECRET,
+      issuer: process.env.ONELOGIN_ISSUER,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST, authOptions };
