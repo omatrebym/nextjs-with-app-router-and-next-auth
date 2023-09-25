1aeb4e9e1d0598928ae2d6a303073386bbd59f80
omatrebym <ola.matre@bym.oslo.kommune.no>
2023-09-25 12:52:40 +0200

    Padding mot header

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="tw-flex tw-flex-col tw-min-h-screen tw-justify-between">
         <Header />
-        <main className="tw-mt-24">{children}</main>
+        <main className="tw-my-0 tw-mx-auto tw-pt-24 tw-pb-16 tablet:tw-pt-28 tablet:tw-px-8 laptop:tw-pt-32">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
