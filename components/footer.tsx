3f7b41480b45dd96f7d37a7d895ed91786cecc9c
omatrebym <ola.matre@bym.oslo.kommune.no>
2023-09-25 13:02:00 +0200

    Fjerne lenke i footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,14 +3,5 @@
 import { PktFooterSimple } from "@oslokommune/punkt-react";
 
 export default function Footer() {
-  return (
-    <PktFooterSimple
-      links={[
-        {
-          href: "https://inntauing.bymoslo.no/",
-          text: "Inntauing",
-        },
-      ]}
-    />
-  );
+  return <PktFooterSimple links={[]} />;
 }
