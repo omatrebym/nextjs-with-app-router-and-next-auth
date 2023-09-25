76fc5ab775f328cacdca429573b5081b1ddcc7c6
omatrebym <ola.matre@bym.oslo.kommune.no>
2023-09-25 15:07:28 +0200

    Bedre feilmeldinger

--- components/logginnknapp.tsx
+++ components/logginnknapp.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { PktAlert, PktButton } from "@oslokommune/punkt-react";
+import { error } from "console";
 import { signIn, useSession } from "next-auth/react";
 import { RedirectType, redirect, useSearchParams } from "next/navigation";
 
@@ -26,9 +27,33 @@ export const LoggInnKnapp = () => {
       </PktButton>
       {error && (
         <PktAlert skin="error" title="Feil ved innlogging">
-          {error}
+          {hentFeilmeldingLoggInn(error)}
         </PktAlert>
       )}
     </div>
   );
+};
+
+const errors = {
+  Signin: "Prøv å logg inn med en annen konto.",
+  OAuthSignin: "Prøv å logg inn med en annen konto.",
+  OAuthCallback: "Prøv å logg inn med en annen konto.",
+  OAuthCreateAccount: "Prøv å logg inn med en annen konto.",
+  EmailCreateAccount: "Prøv å logg inn med en annen konto.",
+  Callback: "Prøv å logg inn med en annen konto.",
+  OAuthAccountNotLinked:
+    "For å bekrefte din identitet, logg inn med den kontoen du brukte opprinnelig.",
+  EmailSignin: "Sjekk eposten din.",
+  CredentialsSignin:
+    "Noe gikk galt under innlogging, sjekk at du har skrevet riktig brukernavn og passord.",
+  default: "Noe gikk galt under innlogging.",
+};
+
+const hentFeilmeldingLoggInn = (feilmeldinger: string) => {
+  const feilmeldingskoder = feilmeldinger.split(",");
+  return feilmeldingskoder.map((feilmeldingskode) => {
+    return (
+      errors[feilmeldingskode.trim() as keyof typeof errors] || errors.default
+    );
+  });
 };
